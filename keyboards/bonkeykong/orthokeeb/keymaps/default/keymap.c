9ace89d2ccb4bac94a3ba608960319d022cf8bd3
patrik werner <patrik.werner@airolit.com>
2025-02-23 17:11:21 +0100

    adds dz60 key map, updates orthokeeb

--- keyboards/bonkeykong/orthokeeb/keymaps/default/keymap.c
+++ keyboards/bonkeykong/orthokeeb/keymaps/default/keymap.c
@@ -167,8 +167,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[L_F1] = LAYOUT(
 		___     , ___       , ___       , ___   , ___       , ___       , ___       , ___       , ___       , ___       , ___       , ___       , ___       , ___       ,
-        KC_F1   , KC_F2     , KC_F3     , KC_F4 , KC_F5     , KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    , KC_NO     , KC_NO     ,
-		QK_GESC , KC_1      , KC_2      , KC_3  , KC_4      , KC_5      , KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_NO     , KC_NO     ,
+		QK_GESC , KC_1      , KC_2      , KC_3  , KC_4      , KC_5      , KC_6      , KC_7      , KC_8      , KC_9      , KC_0      , KC_BSPC   , KC_NO     , KC_NO     ,
+        ___     , KC_F1     , KC_F2     , KC_F3 , KC_F4     , KC_F5     , KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    , KC_F11    , KC_F12    ,
 		KC_LSFT , KC_Z      , KC_X      , KC_C  , KC_V      , KC_B      , KC_MINS   , KC_EQL    , KC_COMM   , KC_DOT    , KC_SLSH   , KC_LSFT   , KC_UP     , KC_BSLS   ,
 		KC_LCTL , KC_LALT   , KC_LGUI   , ___   , ___       , KC_SPC    , ___       , KC_DEL    , KC_LCTL   , KC_LEFT   , KC_DOWN   , KC_RGHT),
 
